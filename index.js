6b6ea0479bff9135e3eca813e857190e0cbb891b
Vansh-Baghel <vanshbaghel912@gmail.com>
2022-12-14 00:09:40 +0530

    Write , read , webServer

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(process.platform);
+const http = require("http");
+
+const server = http.createServer((req , res) => {
+    console.log(req);
+    res.end("Text from server side");
+})
+
+server.listen(8000 , 'localhost' , () => {
+    console.log("Started port");
+})
